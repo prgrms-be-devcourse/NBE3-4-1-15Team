25fc75cd1264e8e20fdab21cd5898040c388d9d9
k-haechan <gibbm1127@naver.com>
2025-01-14 23:55:21 +0900

    feat: domain 엔티티 Member 구현

---
+++ backend/src/main/java/com/nbe/NBE3_4_1_Team15/domain/member/entity/Member.java
@@ -0,0 +1,25 @@
+package com.nbe.NBE3_4_1_Team15.domain.member.entity;
+
+import com.nbe.NBE3_4_1_Team15.global.jpa.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member extends BaseEntity {
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String address;
+
+    private String postCode; // 우편번호
+
+    private MemberType memberType;
+}
