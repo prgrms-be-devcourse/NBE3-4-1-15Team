3e0bba4392ae2d3b539190da24a28d5cfe3aa872
1m1nkim <window12ty@naver.com>
2025-01-16 17:25:35 +0900

    fix: 깃허브 충돌관련 커밋

--- backend/src/main/java/com/nbe/NBE3_4_1_Team15/domain/order/entity/Order.java
+++ backend/src/main/java/com/nbe/NBE3_4_1_Team15/domain/order/entity/Order.java
@@ -1,6 +1,5 @@
 package com.nbe.NBE3_4_1_Team15.domain.order.entity;
 
-import com.nbe.NBE3_4_1_Team15.domain.cart.entity.Cart;
 import com.nbe.NBE3_4_1_Team15.domain.member.entity.Member;
 import com.nbe.NBE3_4_1_Team15.domain.order.type.OrderType;
 import com.nbe.NBE3_4_1_Team15.global.jpa.entity.BaseTime;
@@ -8,10 +7,6 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
-<<<<<<< HEAD
-import java.util.Date;
-=======
->>>>>>> 242d40f3cfa8ba6bfd8a6ada82bc44b42d95e36a
 
 @Entity
 @Getter
@@ -25,21 +20,8 @@ public class Order extends BaseTime {
     private Member consumer; // 주문자 정보 (Member와 다대일 관계)
 
     @Enumerated(EnumType.STRING)
-<<<<<<< HEAD
     private OrderType orderType;
 
-    private Integer totalPrice;
-
-    private LocalDateTime orderDate;
-
-    private LocalDateTime createAt;
-
-    private LocalDateTime updateAt;
-
-=======
-    private OrderType orderType; // 주문 상태 (Enum으로 관리)
->>>>>>> 242d40f3cfa8ba6bfd8a6ada82bc44b42d95e36a
-
     private Integer totalPrice; // 주문 총 금액
-    private LocalDateTime orderDate; // 주문이 이루어진 시간private LocalDateTime orderDate;
+    private LocalDateTime orderDate; // 주문이 이루어진 시간
 }
