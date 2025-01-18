778484478f9e9fa4f93cf74eb0a8a7ffad7411d1
k-haechan <gibbm1127@naver.com>
2025-01-18 15:24:53 +0900

    feat: SecurityUser 구현 (SecurityContext에 저장하는 유저정보 클래스 User를 커스텀한 객체)

---
+++ backend/src/main/java/com/nbe/NBE3_4_1_Team15/global/security/SecurityUser.java
@@ -0,0 +1,23 @@
+package com.nbe.NBE3_4_1_Team15.global.security;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+/**
+ * SpringSecurity 전용 커스텀 클래스
+ * UserDetails를 구현하는 User를 커스텀한 클래스
+ * */
+@Getter
+public class SecurityUser extends User {
+    private final long id;
+    private final String email; // 가독성을 위한 필드(username -> email)
+
+    public SecurityUser(long id, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(email, password, authorities);
+        this.id = id;
+        this.email = email;
+    }
+}
