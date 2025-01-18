a8bcd3fead166b0e1e776369f690bc05882d6cf0
k-haechan <gibbm1127@naver.com>
2025-01-18 15:09:05 +0900

    Rename: MembRepository -> MemberRepository

---
+++ backend/src/main/java/com/nbe/NBE3_4_1_Team15/domain/member/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.nbe.NBE3_4_1_Team15.domain.member.repository;
+
+import com.nbe.NBE3_4_1_Team15.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+//테스트 memberRepository 생성
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String username);
+
+
+}
